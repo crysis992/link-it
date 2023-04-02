0b28dffc84d06b873c6a7057171f0921ec24eb81
crysis992 <crysis992@gmail.com>
2023-04-02 21:27:45 +0200

    Update linkshortener component

--- components/LinkShortener.tsx
+++ components/LinkShortener.tsx
@@ -65,10 +65,14 @@ function LinkShortener() {
             {error.length > 0 && <AlertBox variant="error" title="Error" message={error} />}
             <div className="flex gap-3 mx-auto">
                 <input
-                    className='text-slate-800 bg-slate-100 shadow-sm shadow-slate-200 w-full py-2 rounded-lg px-2 outline-transparent border-b border-slate-400'
+                    className='text-slate-800 bg-slate-100
+                     shadow-sm shadow-slate-200
+                    py-2 rounded-lg px-2  w-full focus:border-2
+                    focus:border-green-500 transition focus:outline-none
+                    '
                     type="url"
                     autoComplete="off"
-                    placeholder="Paste an URL to shorten (Example: https://www.twitch.tv/spaschi)"
+                    placeholder="Paste an URL to shorten"
                     value={data}
                     onChange={(e) => setData(e.target.value)}
                 />
